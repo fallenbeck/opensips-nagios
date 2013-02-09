1e98635d6c60eb11f92458956fcda3729d990f9c
Niels Fallenbeck <niels@fallenbeck.com>
2013-02-10 00:31:03 +0100

    Now we can start to parse opensipsctls output...
    Signed-off-by: Niels Fallenbeck <niels@fallenbeck.com>

--- opensips.py
+++ opensips.py
@@ -35,7 +35,8 @@
 import sys
 import os
 import optparse
-# from optparse import OptionParser
+from subprocess import PIPE
+from subprocess import Popen
 
 
 class OpenSipsModule:
@@ -44,34 +45,63 @@ class OpenSipsModule:
 	# opensipsctl = '/usr/local/bin/opensips/sbin/opensipsctl' # production
 	opensipsctl = './opensipsctl_dummy' # opensipsctl dummy for testing purposes
 
-	options = ''
+	# default settings
+	threshold_warning = 2
+	threshold_critical = 5
+	exitstatus = 3 #OpenSipsModuleStates.UNKNOWN
+	test = False
+	metric = "all"
 
-	def parseOptions(self):
+
+	def parse_options(self):
 		from optparse import OptionParser
 		parser = OptionParser()
-		# parser.add_option("-h", "--help", help="print this help")
-		parser.add_option("-v", "--version", help="print version and exit")
-		parser.add_option("-M", "--metric", dest="metric", help="metric to show")
+		parser.add_option("-v", "--version", action="store_true", dest="show_version", default=False, help="print version and exit")
+		parser.add_option("-t", "--test", action="store_true", dest="test_config", default=False, help="test if configuration is working")
+		parser.add_option("-M", "--metric", dest="metric", default="all", help="metric to show")
 		parser.add_option("-x", "--exitstatus", dest="state", help="exit with this state")
 		parser.add_option("-w", "--warning", dest="threshold_warning", help="set threshold for warning state")
 		parser.add_option("-c", "--critical", dest="threshold_critical", help="set threshold for critical state")
 		(self.options, args) = parser.parse_args()
 
 
-	def printVersion(self):
+	def print_version(self):
 		print '%s %s' % (self.name, self.version)
 
-	def printDebugInfo(self):
-		self.printVersion()
-		print 'Binary used: %s' % self.opensipsctl
 
-	def checkEnvironment(self):
+	def check_config(self):
 		# check if specified binary can be used
-		print ''
+		print 'check system environment...'
+		print '  binary used as configured in script: %s' % self.opensipsctl
+		try:
+   			with open(self.opensipsctl) as f: 
+				p = Popen('%s 1 2 all' % self.opensipsctl, stdout=PIPE, stderr=PIPE, shell=True)
+				output, errors = p.communicate()
+				if p.returncode:
+					print 'Error: %s' % errors
+					exit(2)
+				else:
+					print '  opensipsctl binary found.'
+		except IOError as e:
+   			print 'Error: %s' % e
+   			exit(1)
+
+
+   	def get_metric(self):
+   		print "The metric (%s) happens here!" % self.metric
+
 
 	def __init__(self):
-		self.printDebugInfo()
-		self.parseOptions()
+		self.parse_options()
+
+		if self.options.show_version:
+			self.print_version()
+		elif self.options.test_config:
+			self.print_version()
+			self.check_config()
+		else:
+			self.get_metric()
+
 		sys.exit(OpenSipsModuleStates.OK)
 
 
